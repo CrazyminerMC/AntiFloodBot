707d5509e01f5176a797a7f640e20e592ed359d2
Michele <micheleviotto.13@gmail.com>
2020-06-16 22:10:01 +0200

    Aggiunti comandi /punisci e /banhammer (vedi issue #1)
    Sono stati risolti anche problemi minori

--- AntiFloodBot.py
+++ AntiFloodBot.py
@@ -2,10 +2,13 @@
 import os.path
 import random
 import time
+from datetime import datetime as dt, timedelta
 
 import botogram
 import yaml
 import requests
+
+import database as db
 
 config = yaml.safe_load(open('config.yml'))
 
@@ -33,6 +36,8 @@ ytinstances = config.get('ytinstances')
     "(.*(?:watch|embed)?(?:.*v=|v\\/|\\/)[\\w_-]+))",
     multiple=True)
 def youtube_link_replace(message, matches):
+    db.add_user(message.sender.id, message.sender.username)
+    db.update_username(message.sender)
     if len(matches) == 2:
         yt_instance_url = get_working_yt_instance(escape(matches[1]))
         if yt_instance_url:
@@ -47,6 +52,8 @@ def youtube_link_replace(message, matches):
     "(?:\\/status\\/\\d+)?))",
     multiple=True)
 def twitter_link_replace(message, matches):
+    db.add_user(message.sender.id, message.sender.username)
+    db.update_username(message.sender)
     if len(matches) == 2:
         blip_blopper(message, "https://nitter.net/", matches)
 
@@ -65,6 +72,8 @@ def delete_message(query, data, chat, message):
 # antiflood
 @bot.process_message
 def antiflood(shared, chat, message):
+    db.add_user(message.sender.id, message.sender.username)
+    db.update_username(message.sender)
     if 'users' not in shared:
         if os.path.isfile('data.yml'):
             shared['users'] = yaml.safe_load(open('data.yml')).get('users')
@@ -175,6 +184,153 @@ def captcha_callback(shared, query, data, chat, message):
                              attach=generate_captcha_buttons())
 
 
+# punisce un utente
+# 1 volta: muta utente per una settimana
+# 2 volta: ban permanente, se è passato un anno funziona come la prima volta
+@bot.command("punisci")
+def punisci_command(chat, message, args):
+    if chat.type not in ("group", "supergroup"):
+        return
+
+    if message.sender not in chat.admins:
+        return
+
+    if message.reply_to_message:
+        user_to_punish = message.reply_to_message.sender
+        db.add_user(user_to_punish.id, user_to_punish.username)
+        punishment = punish_user(user_to_punish, chat)
+
+        # l'utente è stato silenziato
+        if punishment == 0:
+            text = f"L'utente {get_user_tag(user_to_punish)} è stato silenziato per 7 giorni."
+
+            if args:
+                punishment_reason = " ".join(args)
+                text = text + f"\nMotivo: {punishment_reason}"
+
+            chat.send(text)
+            return
+
+        # l'utente è stato bannato
+        if punishment == 1:
+            text = f"L'utente {get_user_tag(user_to_punish)} è stato rimosso."
+
+            if args:
+                punishment_reason = " ".join(args)
+                text = text + f"\nMotivo: {punishment_reason}"
+
+            chat.send(text)
+            return
+
+    if len(args) >= 1:
+        is_username = True  # check if args[0] is username or id
+        if '@' not in args[0]:
+            is_username = False
+
+        if is_username:
+            user = db.get_user_by_username(args[0].replace('@', ''))
+            if user:
+                user_id = user[0]
+            else:
+                return
+            username = args[0]
+        else:
+            user_id = args[0]
+            username = args[0]
+
+        del args[0]
+        punishment = punish_user(user_id, chat)
+        # l'utente è stato silenziato
+        if punishment == 0:
+            text = f"L'utente [{username}](tg://user?id={user_id}) è stato silenziato per 7 giorni."
+
+            if args:
+                punishment_reason = " ".join(args)
+                text = text + f"\nMotivo: {punishment_reason}"
+
+            chat.send(text)
+            return
+
+        # l'utente è stato bannato
+        if punishment == 1:
+            text = f"L'utente [{username}](tg://user?id={user_id}) è stato rimosso."
+
+            if args:
+                punishment_reason = " ".join(args)
+                text = text + f"\nMotivo: {punishment_reason}"
+
+            chat.send(text)
+            return
+
+
+
+# banhammer per casi eccezionali
+@bot.command("banhammer")
+def banhammer_command(chat, message, args):
+    if chat.type not in ("group", "supergroup"):
+        return
+
+    if message.sender not in chat.admins:
+        return
+
+    if message.reply_to_message:
+        user_to_ban = message.reply_to_message.sender
+
+        if not args:
+            chat.ban(user_to_ban)
+            chat.send(f"L'utente {get_user_tag(user_to_ban)} è stato rimosso.")
+            db.remove_user(user_to_ban.id)
+            return
+
+        ban_reason = " ".join(args)
+        chat.ban(user_to_ban)
+        chat.send(
+            f"L'utente {get_user_tag(user_to_ban)} è stato rimosso.\n"
+            f"Motivo: {ban_reason}"
+        )
+        db.remove_user(user_to_ban.id)
+        return
+
+    # il comando non è usato in risposta e quindi richiede un id o username
+    if len(args) >= 1:
+        is_username = True  # check if args[0] is username or id
+        if '@' not in args[0]:
+            is_username = False
+
+        if is_username:
+            user = db.get_user_by_username(args[0].replace('@', ''))
+
+            del args[0]
+
+            chat.ban(user[0])
+            if not args:
+                chat.send(f"L'utente @{user[1]} è stato rimosso.")
+            else:
+                ban_reason = " ".join(args)
+                chat.send(
+                    f"L'utente @{user[1]} è stato rimosso.\n"
+                    f"Motivo: {ban_reason}"
+                )
+            db.remove_user(user[0])
+        else:
+            user = args[0]
+
+            del args[0]
+
+            chat.ban(user)
+            if not args:
+                chat.send(f"L'utente {user} è stato rimosso.")
+            else:
+                ban_reason = " ".join(args)
+                chat.send(
+                    f"L'utente {user} è stato rimosso.\n"
+                    f"Motivo: {ban_reason}"
+                )
+            db.remove_user(user)
+
+        return
+
+
 # generate captcha buttons
 def generate_captcha_buttons():
     btns = botogram.Buttons()
@@ -222,7 +378,7 @@ def get_user_tag(user):
     if user.username:
         return '@' + escape(user.username)
     else:
-        return '[%s](tg:///user?id=%d)' \
+        return '[%s](tg://user?id=%d)' \
                % (escape(user.first_name), user.id)
 
 
@@ -241,6 +397,68 @@ def removekey(d, key):
 def saveusers(users):
     with open('data.yml', 'w') as file:
         yaml.dump({'users': users}, file)
+
+
+# legenda dei return
+# 0: l'utente non ha warn
+# -1: l'utente ha un warn ma è più vecchio di un anno (31536000.0 secondi)
+# 1: l'utente ha un warn ed è più recente di un anno (31536000.0 secondi)
+def check_punishment(user_id):
+    user = db.get_user(user_id)
+    warnings = user[2]
+    last_warn = user[3]
+    more_than_one_year = (time.time() - float(last_warn) >= 31536000.0) if last_warn else True
+
+    if warnings == 0:
+        return 0
+
+    if warnings == 1 and more_than_one_year:
+        return -1
+
+    return 1
+
+
+# ritorna 0 se l'utente viene silenziato
+# ritorna 1 se l'utente viene bannato
+def punish_user(user, chat):
+    if isinstance(user, botogram.User):
+        user_id = user.id
+    else:
+        user_id = user
+
+    punishment = check_punishment(user_id)
+
+    # silenzia utente e aggiunge warn nel db
+    if punishment == 0:
+        with chat.permissions(user_id) as perms:
+            perms.send_messages = False
+            perms.send_media_messages = False
+            perms.send_other_messages = False
+            perms.add_web_page_previews = False
+            perms.until_date = dt.now() + timedelta(days=7)
+            perms.save()
+
+        db.set_warning(user_id, 1)
+        return 0
+
+    # silenzia utente e cambia la data nel database con quella attuale
+    if punishment == -1:
+        with chat.permissions(user_id) as perms:
+            perms.send_messages = False
+            perms.send_media_messages = False
+            perms.send_other_messages = False
+            perms.add_web_page_previews = False
+            perms.until_date = dt.now() + timedelta(days=7)
+            perms.save()
+
+        db.set_warning(user_id, 1)
+        return 0
+
+    # banna permanentemente l'utente e lo rimuove dal db
+    if punishment == 1:
+        chat.ban(user_id)
+        db.remove_user(user_id)
+        return 1
 
 
 if __name__ == "__main__":
